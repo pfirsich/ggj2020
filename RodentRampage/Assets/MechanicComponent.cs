9ec804e37e5006cc93d9d664d3cc1dd2d1656e60
Carolina Schmitz <carolinaschmitz@rocketmail.com>
2020-02-01 18:47:07 +0100

    Add picking up tools

--- RodentRampage/Assets/MechanicComponent.cs
+++ RodentRampage/Assets/MechanicComponent.cs
@@ -5,14 +5,45 @@ using UnityEngine.InputSystem;
 
 public class MechanicComponent : MonoBehaviour
 {
-    public GameObject attachedTool;
+    public ToolComponent pickedUpTool;
+
     private PlayerInput input;
-    // Start is called before the first frame update
+    private ToolComponent touchingTool;
+
     void Start()
     {
         input = GetComponent<PlayerInput>();        
     }
 
+    void OnThrow(InputValue inputValue)
+    {
+        if(!pickedUpTool && touchingTool && !touchingTool.pickedUpBy)
+        {
+            pickedUpTool = touchingTool;
+            touchingTool.pickedUpBy = this;
+            touchingTool.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            touchingTool.gameObject.GetComponent<Rigidbody>().detectCollisions = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var tool = other.GetComponent<ToolComponent>();
+        if (tool)
+        {
+            touchingTool = tool;
+            
+        }
+    }
+
+    void onTriggerExit(Collider other)
+    {
+        var tool = other.GetComponent<ToolComponent>();
+        if (tool && touchingTool == tool)
+        {
+            touchingTool = null;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
---
+++ RodentRampage/Assets/ToolComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolComponent : MonoBehaviour
+{
+    public enum ToolType { Hammer, Wrench, Screwdriver, Drill, Oilcan };
+    public ToolType toolType;
+    public MechanicComponent pickedUpBy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickedUpBy)
+        {
+            transform.localPosition = pickedUpBy.transform.localPosition;
+        }
+    }
+}
