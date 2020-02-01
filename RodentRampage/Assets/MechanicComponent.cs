70dd38c5de8a77c317eef24e86213fc3ee360e1c
Carolina Schmitz <carolinaschmitz@rocketmail.com>
2020-02-01 17:50:40 +0100

    Add beginning of tools

---
+++ RodentRampage/Assets/MechanicComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MechanicComponent : MonoBehaviour
+{
+    public GameObject attachedTool;
+    private PlayerInput input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<PlayerInput>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
