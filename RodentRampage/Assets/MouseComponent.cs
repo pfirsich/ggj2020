f80a5c1ce6ba34c246313b725cf7c27e73cd3f99
Carolina Schmitz <carolinaschmitz@rocketmail.com>
2020-02-02 14:17:11 +0100

    add Sounds for tools and machine repair. need to add background music and repair and break sound to each machine

--- RodentRampage/Assets/MouseComponent.cs
+++ RodentRampage/Assets/MouseComponent.cs
@@ -42,8 +42,7 @@ public class MouseComponent : MonoBehaviour
                 lastGnaw = currentTime;
             }
         }
-        //if(gnawsound....)
-        //    if gnawing play sound
+
     }
 
     void OnUse(InputValue value)
