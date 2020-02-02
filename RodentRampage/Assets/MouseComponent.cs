ec945f0b03aa81bf9f08775eea0c34a9f4783347
Carolina Schmitz <carolinaschmitz@rocketmail.com>
2020-02-02 12:31:21 +0100

    Add mouse sounds

--- RodentRampage/Assets/MouseComponent.cs
+++ RodentRampage/Assets/MouseComponent.cs
@@ -12,20 +12,50 @@ public class MouseComponent : MonoBehaviour
     private bool gnawing = false;
     private MachineComponent touchingMachine = null;
 
+    public float currentTime;
+    AudioSource gnawSound;
+    public float gnawIntervall = 0.6f;
+    public float lastGnaw = 0.0f;
+    public float timeSinceLastGnaw;
+
+
+
     void Start()
     {
         input = GetComponent<PlayerInput>();
+        gnawSound = GetComponent<AudioSource>();
+        currentTime = Time.time;
+        lastGnaw = 0.0f ;
     }
 
     void Update()
     {
-        if (touchingMachine && gnawing)
+        if (touchingMachine && gnawing) {
             touchingMachine.gnaw(gnawSpeed * Time.deltaTime);
+
+            currentTime = Time.time;
+            timeSinceLastGnaw = currentTime - lastGnaw;
+            if (timeSinceLastGnaw > gnawIntervall)
+            {
+                Debug.Log("Time " + currentTime);
+                gnawSound.Play(0);
+                lastGnaw = currentTime;
+            }
+        }
+        //if(gnawsound....)
+        //    if gnawing play sound
     }
 
     void OnUse(InputValue value)
     {
         gnawing = value.Get<float>() > 0.5f;
+        //hier den sound nach nem zeit intervall Time.time
+        //TODO check if at machine  
+        if (touchingMachine && gnawing)
+        {
+            gnawSound.Play(0);
+            lastGnaw = Time.time;
+        }
     }
 
     void OnTriggerEnter(Collider other)
